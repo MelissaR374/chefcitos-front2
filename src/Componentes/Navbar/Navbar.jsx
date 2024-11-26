05dd234e12ece88438412e3a561c4cac3e3c3bf6
Melissa Ruiz <melissa.ruizglez.3d@gmail.com>
2024-11-26 03:41:54 -0600

    Menú funcionando, falta corregir el menú de añadir receta porque se sobrepone una parte del header en el menú

--- src/Componentes/Navbar/Navbar.jsx
+++ src/Componentes/Navbar/Navbar.jsx
@@ -2,6 +2,13 @@ import React, {useState, useEffect} from 'react';
 import './Navbar.css';
 import IconoHam from '/src/assets/hamburguesa.png';
 import IconoCoct from '/src/assets/copa-de-martini-citricos.png';
+import barras from '/src/assets/clasificacion-de-barras.png';
+import corazonfav from '/src/assets/fav.png';
+import raton from '/src/assets/raton.png';
+import libro from '/src/assets/libro-marcador.png'
+import add from '/src/assets/mas.png'
+import entrar from '/src/assets/iniciar-sesion.png'
+import salir from '/src/assets/cerrar-sesion.png'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -26,14 +33,44 @@ const Navbar = () => {
     //agregar función modo oscuro (interfaz de cocteleria)
   };
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className='navbar'>
-      <div className='menu'>
+      <div className='menu-icon' onClick={toggleMenu}>
         <i class="fi fi-br-menu-burger"></i>
       </div>
       <Link to="/">
         <div className='logo'>Chefcitos</div>
       </Link>
+
+      <nav className={`sidebar ${menuOpen ? 'open' : ''}`}>
+        <img src={barras} alt="Barras" className='icon-barras-menu'onClick={toggleMenu}/>
+        <ul>
+          <li>
+            <Link to="/recetas-favoritas"><button className="fav"><img src={corazonfav} alt="Fav" className='icon-corazonfav' />Favoritos</button></Link>
+          </li>
+          <li>
+            <Link to="/perfil"><button className="perfil"><img src={raton} alt="Perfil" className='icon-raton' />Perfil</button></Link>
+          </li>
+          <li>
+            <Link to="/recetas-creadas"><button className="mine"><img src={libro} alt="Misrecetas" className='icon-recetario' />Mis recetas</button></Link>
+          </li>
+          
+        </ul>
+        <Link to="/agregar-receta">
+          <button className="add-recipe"><img src={add} alt="add" className='icon-agregar' />Añadir receta</button>
+        </Link>
+        
+        <button className="login"><img src={entrar} alt="entrar" className='icon-entrar'/>Iniciar sesión</button>
+      </nav>
+
+      {/* Fondo oscuro al abrir el menú */}
+      {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
 
       <div className="busqueda">
         <input className='buscar' type="text" placeholder={placeholders[placeholderIndex]}/>
--- src/Componentes/agregar-recetas/Header.jsx
+++ src/Componentes/agregar-recetas/Header.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import './Header.css';
 import cubiertos from '/src/assets/cubiertos.png';
+import barras from '/src/assets/clasificacion-de-barras.png';
+import corazonfav from '/src/assets/fav.png';
+import raton from '/src/assets/raton.png';
+import libro from '/src/assets/libro-marcador.png'
+import add from '/src/assets/mas.png'
+import entrar from '/src/assets/iniciar-sesion.png'
+import salir from '/src/assets/cerrar-sesion.png'
+
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -12,20 +20,49 @@ const Header = () => {
         setSelectedCategory(category); // Actualiza la categoría seleccionada
     };
 
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <div className="header">
             <div className="header-left">
-                <div className="menu-btn">
+                <div className="menu-btn" onClick={toggleMenu}>
                     <i className="fi fi-br-menu-burger"></i>
                 </div>
                 <Link to="/">
                     <h1 className='logo'>Chefcitos</h1>
                 </Link>
+                <nav className={`sidebar ${menuOpen ? 'open' : ''}`}>
+                    <img src={barras} alt="Barras" className='icon-barras-menu'onClick={toggleMenu}/>
+                    <ul>
+                        <li>
+                            <Link to="/recetas-favoritas"><button className="fav"><img src={corazonfav} alt="Fav" className='icon-corazonfav' />Favoritos</button></Link>
+                        </li>
+                        <li>
+                            <Link to="/perfil"><button className="perfil"><img src={raton} alt="Perfil" className='icon-raton' />Perfil</button></Link>
+                        </li>
+                        <li>
+                            <Link to="/recetas-creadas"><button className="mine"><img src={libro} alt="Misrecetas" className='icon-recetario' />Mis recetas</button></Link>
+                        </li>
+                    
+                    </ul>
+                    <Link to="/agregar-receta">
+                    <button className="add-recipe"><img src={add} alt="add" className='icon-agregar' />Añadir receta</button>
+                    </Link>
+                    
+                    <button className="login"><img src={entrar} alt="entrar" className='icon-entrar'/>Iniciar sesión</button>
+                </nav>
+                {/* Fondo oscuro al abrir el menú */}
+                {menuOpen && <div className="overlay" onClick={toggleMenu}></div>}
             </div>
 
             <div className="header-right">
                 <button className="botonred">Añadir</button>
                 <button className="botorange">Vista previa</button>
+                 
             </div>
             <br />
 
@@ -52,6 +89,7 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
+                 
             </div>
         </div>
     );
